e44521cef0e09621a577f85923c3b0441eedbdb4
diksha6036 <malikdiksha385@gmail.com>
2022-04-12 00:15:13 +0530

    commit to develop branch

---
+++ cn.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int fab=45;
+
+    int d;
+
+
+int main(){
+    int fab=23;
+    fab=34;
+    char ='a';
+    // int a,b;
+    // string c;
+    // cout<<"Which opertaion you want to perform (+,-,*,/,%,**,//)";
+    // cin>>c;
+    // cout<<"Enter  your first number";
+    // cin>>a;
+    // cout<<"Enter your second number";
+    // cin>>b;
+    // if (c == '**'){
+    //   cout<<a**b;
+    // }
+    cout<<fab<<"\n";
+    cout<<d;
+
+
+    return 0;
+}
